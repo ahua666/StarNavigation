e8eee70ddd4164e3a7459de69c839843dc695086
Star <2524329629@qq.com>
2023-03-30 21:04:37 +0800

    feat:initialization💦💦

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "Star导航",
+  lang: "zh-CN",
+  description: "软件开发导航",
+  lastUpdated: true,
+  cleanUrls: true,
+  /* markdown 配置 */
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    logo: "/logo.png",
+    siteTitle: "Star导航",
+    /* 右侧大纲配置 */
+    outline: {
+      level: "deep",
+      label: "本页目录",
+    },
+    socialLinks: [{ icon: "github", link: "https://github.com/ahua666" }],
+    footer: {
+      message: 'Github开源项目',
+      copyright: 'Copyright © 2023 Star'
+    },
+    darkModeSwitchLabel: '外观',
+    returnToTopLabel: '返回顶部',
+    lastUpdatedText: '上次更新',
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇'
+    }
+  },
+
+});
---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+layoutClass: m-nav-layout
+outline: [2, 3, 4]
+---
+
+# 导航
+
+
+<style src="./nav/nav.scss"></style>
+
+<Navlink></Navlink>
